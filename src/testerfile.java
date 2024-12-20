d794ca1e1bda507e2a6d783bffee0d087edb58c7
PVONOM <shinwatch1019@gmail.com>
2024-12-20 05:56:03 -0500

    Update testerfile.java

--- src/testerfile.java
+++ src/testerfile.java
@@ -1,6 +1,6 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 import javax.swing.*;
+import java.math.*;
 
 public class testerfile {
     public static void main(String[] args){
@@ -9,6 +9,9 @@ public class testerfile {
         String rowC = new String("????");
         String colC = new String("????");
 
+        /*
+         * accepts the RxC dimentions 
+         */
         while (!jerry) {
             System.out.println( "enter the number of rows of ur matrix " );
             rowC = scanner.nextLine();
@@ -27,7 +30,7 @@ public class testerfile {
         System.out.println("success");
         System.out.println("ur matrix is of " + rowC + "x" + colC);
         
-        int[][] matrix = new int[Integer.parseInt(rowC)][Integer.parseInt(colC)];
+        int[][] matrix = new int[Integer.parseInt(rowC)][Integer.parseInt(colC)]; // creates an matrix of said dimentions 
 
         for(int i =1; i <= Integer.parseInt(rowC); i++){
             System.out.println("enter the values of row " + i);
@@ -42,36 +45,42 @@ public class testerfile {
         }
         
 
+        System.out.println("success");
         print2D(matrix);
         jerry = false;
 
         System.out.println("what do you want to do with your matrix?? "); String input; String det = new String("det");String none = new String("none");
         while (!jerry) {
-            System.out.println("print 'det' to find the determinent and print 'none' to end the program ");
+            System.out.println("print 'det' to find the determinent and print 'none' to end the program (case sensitive!)");
             input = scanner.nextLine();
             if(input.equals(none))
                 jerry = true;
-            
-            if(input.equals(det)){
-                int dup = determi(matrix , Integer.parseInt(rowC), Integer.parseInt(colC));
-                System.out.println("the determinent of this matrix is: " + dup);
+            else if(input.equals(det)){
+                determi(matrix , Integer.parseInt(rowC), Integer.parseInt(colC));
+                jerry = true;
+            }else{
+                System.out.println("that was not one of the options! D:<");
             }
+                
+            
+            
         }
 
         scanner.close();
     } // end main
 
-    public static int determi(int mat[][], int row , int col){
-        int determinent = -10000000;
+    /*
+     * to find the determinant
+     */
+    public static void determi(int mat[][], int row , int col){
         if( row != col){
             System.out.print("only square matrices can have a determinent!!");
-            return -10000000;
         }
         if( row ==2){
-            determinent = twoByTwo(mat);
-            return determinent;
+            System.out.println("the determinent of this matrix is  " + twoByTwo(mat));
+        }else {
+            System.out.println("the determinent of this matrix is  " + recurs(mat, row));
         }
-
         /*int[][][] ahah = new int[row-1][row-1][row];
         if(row > 2){
             for(int z = 0; z < row; z++){
@@ -83,7 +92,7 @@ public class testerfile {
             } IDEA OF A 3D MATRIX 
         }*/
 
-        if(row == 3){
+        /*if(row == 3){
             int[][] won = new int[2][2];
             int[][] tue = new int[2][2];
             int[][] tree = new int[2][2];
@@ -109,12 +118,40 @@ public class testerfile {
             determinent = (mat[0][0]*determi(won,2,2)) - (mat[1][0]*determi(tue,2,2)) + (mat[2][0]*determi(tree,2,2));
             return determinent;
 
+        }*/
+
+    }
+
+    public static int recurs(int mat[][], int size){
+        if(size == 2){
+            return twoByTwo(mat);
         }
+        double det = 0;
+        int[][] pat = new int[size-1][size-1];
+
+
+        for(int i = 0; i< size; i++){// the counter for the first row of a11 a12 ... etc;
+
+            for(int x = 0; x <size-1; x++ ){
+                int z = 0;
+                for(int y = 0; y < size-1; y++){
+                    // make it so that the value only gets inputed if y!= i
+                    if(z == i)
+                        z++;
+                    
+                    pat[x][y] = mat[x+1][z];
+                    z++;
+                }
+            }
 
 
 
 
-        return determinent;
+
+            det = det+ (Math.pow(-1, 1+i)*mat[0][i]*recurs(pat, size-1));
+        }
+        int caca = (int) det;
+        return -caca; // got no clue why the det becomes neg but oh well
     }
 
     public static int twoByTwo(int mat[][]){
@@ -128,9 +165,6 @@ public class testerfile {
     {
         // Loop through all rows
         for (int[] row : mat)
-
-            // converting each row as string
-            // and then printing in a separate line
             System.out.println(Arrays.toString(row));
     }
 
